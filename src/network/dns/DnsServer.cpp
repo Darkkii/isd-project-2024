4d776635de4a148679bd35643abb0e80509d45b5
Darkkii <anton_saarinen@hotmail.com>
2025-01-29 22:37:04 +0200

    code refactoring and cleanup

--- src/network/dns/DnsServer.cpp
+++ src/network/dns/DnsServer.cpp
@@ -1,36 +1,41 @@
 #include "DnsServer.hpp"
 
 #include "DnsCommon.hpp"
+#include "network/udp/UdpCallback.hpp"
 #include <lwip/err.h>
 #include <lwip/ip4_addr.h>
 
-// #include <array>
 #include <cstdint>
 #include <cstdio>
+#include <cstring>
 #include <iterator>
 
 namespace Network::Dns
 {
 
-DnsServer::DnsServer(const std::string &serverIp)
+DnsServer::DnsServer(const std::shared_ptr<std::string> serverIp)
 {
-    m_Udp = udp_new();
-    ip4addr_aton(serverIp.c_str(), ip_2_ip4(&m_Ip));
+    constexpr uint16_t PORT_DNS_SERVER = 53;
 
-    udp_recv(m_Udp, udpReceive, this);
+    ip4addr_aton(serverIp->c_str(), ip_2_ip4(&m_Ip));
 
-    udp_bind(m_Udp, IP4_ADDR_ANY, PORT_DNS_SERVER);
+    m_UdpPcb = udp_new();
+    udp_recv(m_UdpPcb, Udp::udpReceive, this);
+
+    ip_addr_t addr;
+    ip4_addr_set_zero(&addr);
+
+    udp_bind(m_UdpPcb, &addr, PORT_DNS_SERVER);
 
     printf("DNS server listening on port %d\n", PORT_DNS_SERVER);
 }
 
 DnsServer::~DnsServer()
-
 {
-    if (m_Udp != nullptr)
+    if (m_UdpPcb != nullptr)
     {
-        udp_remove(m_Udp);
-        m_Udp = nullptr;
+        udp_remove(m_UdpPcb);
+        m_UdpPcb = nullptr;
     }
 }
 
@@ -38,13 +43,9 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
 {
     constexpr size_t DNS_HEADER_SIZE = DNS_MESSAGE_LEN_MAX - DNS_RECORDS_LEN_MAX;
 
-    printf("dns_server_process %u\n", p->tot_len);
-
     (void)nif;
 
-    // std::array<uint8_t, MAX_DNS_MSG_SIZE> message;
     DnsMessage message;
-    // auto *header = (dns_header_t *)message.data();
     size_t len = pbuf_copy_partial(p, &message, sizeof(message), 0);
     uint16_t flags = lwip_ntohs(message.flags);
     uint16_t qCount = lwip_ntohs(message.question_count);
@@ -83,11 +84,7 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
         return -1;
     }
 
-    // Print the question
     printf("question: ");
-    // const uint8_t *question_ptr_start = message.data() +
-    // sizeof(dns_header_t); const uint8_t *question_ptr_end = message.data() +
-    // message.size(); const uint8_t *question_ptr = question_ptr_start;
     auto *recordsStart = std::cbegin(message.records);
     auto *recordsEnd = std::cend(message.records);
     auto *currentRecord = recordsStart;
@@ -105,7 +102,7 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
             int label_len = *currentRecord++;
             if (label_len > 63)
             {
-                printf("Invalid label\n");
+                printf("[Error %d] - Invalid label.\n", -1);
                 pbuf_free(p);
                 return -1;
             }
@@ -119,7 +116,7 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
     // Check question length
     if (std::distance(recordsStart, currentRecord) > 255)
     {
-        printf("Invalid question length\n");
+        printf("[Error %d] - Invalid question length\n", -1);
         pbuf_free(p);
         return -1;
     }
@@ -128,13 +125,9 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
     currentRecord += 4;
 
     // Generate answer
-    // uint8_t *answer_ptr = message.data() + (question_ptr - message.data());
-    // uint8_t *answer = message.records + (question - message.records);
-
     uint8_t index = std::distance(recordsStart, currentRecord);
     message.records[index++] = 0xc0; // pointer
     message.records[index++] = DNS_HEADER_SIZE;
-    // *answer++ = recordsStart - message.records; // pointer to question
 
     message.records[index++] = 0;
     message.records[index++] = 1; // host address
@@ -148,8 +141,8 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
     message.records[index++] = 60; // ttl 60s
 
     message.records[index++] = 0;
-    message.records[index++] = 4;                                 // length
-    memcpy(&message.records[index], &ip4_addr_get_u32(&m_Ip), 4); // use our address
+    message.records[index++] = 4;                                      // length
+    std::memcpy(&message.records[index], &ip4_addr_get_u32(&m_Ip), 4); // use our address
     index += 4;
 
     message.flags = lwip_htons(0x1 << 15 | // QR = response
@@ -165,16 +158,37 @@ int DnsServer::process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif
            sizeof(message) - (recordsEnd - &message.records[index]),
            ipaddr_ntoa(src_addr),
            src_port);
-    udpSend(m_Udp,
-            nullptr,
-            &message,
-            sizeof(message) - (recordsEnd - &message.records[index]),
-            ip4_addr_get_u32(src_addr),
-            src_port);
+
+    send(&message, sizeof(message) - (recordsEnd - &message.records[index]), src_addr, src_port);
 
     pbuf_free(p);
 
     return ERR_OK;
+}
+
+int DnsServer::send(const void *buf, size_t len, const ip_addr_t *dest, uint16_t port)
+{
+    if (len > 0xffff) { len = 0xffff; }
+
+    struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, len, PBUF_RAM);
+    if (p == nullptr)
+    {
+        printf("DNS: Failed to send message out of memory\n");
+        return -ENOMEM;
+    }
+
+    memcpy(p->payload, buf, len);
+    err_t err = udp_sendto(m_UdpPcb, p, dest, port);
+
+    pbuf_free(p);
+
+    if (err != ERR_OK)
+    {
+        printf("DNS: Failed to send message %d\n", err);
+        return err;
+    }
+
+    return len;
 }
 
 } // namespace Network::Dns
--- src/network/dns/DnsServer.hpp
+++ src/network/dns/DnsServer.hpp
@@ -1,25 +1,28 @@
 #ifndef DNSSERVER_HPP
 #define DNSSERVER_HPP
 
-#include "lwip/ip_addr.h"
-#include "network/NetworkCommon.hpp"
+#include "network/udp/UdpServer.hpp"
+#include <lwip/ip_addr.h>
 #include <lwip/udp.h>
 
+#include <memory>
 #include <string>
 
 namespace Network::Dns
 {
 
-class DnsServer : public UdpServer
+class DnsServer : public Udp::UdpServer
 {
   public:
-    DnsServer(const std::string &serverIp);
+    DnsServer(const std::shared_ptr<std::string> serverIp);
+    DnsServer(const DnsServer &) = delete;
     ~DnsServer();
     int process(pbuf *p, const ip_addr_t *src_addr, u16_t src_port, netif *nif) override;
 
   private:
     ip_addr_t m_Ip{};
-    udp_pcb *m_Udp{};
+    udp_pcb *m_UdpPcb{};
+    int send(const void *buf, size_t len, const ip_addr_t *dest, uint16_t port);
 };
 
 } // namespace Network::Dns
